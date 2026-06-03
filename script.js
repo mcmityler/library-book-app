187b36ea8392d830475777b031443b7192e4f462
Tyler McMillan <36271467+mcmityler@users.noreply.github.com>
2026-06-03 09:47:28 -0400

    Add JS to loop through books to add divs to html
    Also added basic html and css to get a feel for the layout.

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const myLibrary = []
+const myLibrary = [];
 
 function Book(title, author, pages, haveRead) {
     if (!new.target) {
@@ -17,6 +17,23 @@ function addBookToLibrary(title, author, pages, haveRead) {
   myLibrary.push(newBook);
 }
 
-console.log(myLibrary[0]);
-addBookToLibrary("Bone: The Great Cow Race", "Jeff Smith", 132, true)
-console.log(myLibrary[0]);
+
+addBookToLibrary("Bone: The Great Cow Race", "Jeff Smith", 132, true);
+addBookToLibrary("Lord of the Flies", "William Golding", 260, true);
+addBookToLibrary("1984", "George Orwell", 300, false);
+addBookToLibrary("Romeo and Juliet", "William Shakespeare ", 125, true);
+addBookToLibrary("The Martian", "Andy Weir", 448, false);
+console.table(myLibrary);
+
+const container = document.querySelector(".library-section");
+
+myLibrary.forEach(book => {
+   // 1. Create the new div element
+  const newBookDiv = document.createElement("div");
+  // 2. Add content, classes, or IDs
+  newBookDiv.textContent = `${book.title}`;
+  newBookDiv.classList.add("book-div");
+
+  // 3. Append it to an existing container (like the body)
+  container.appendChild(newBookDiv);
+});
