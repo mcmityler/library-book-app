f5880112d96cfd32ced7a19f55cbbeab80db8c0c
Tyler McMillan <36271467+mcmityler@users.noreply.github.com>
2026-06-05 20:26:02 -0400

    Rework javascript to add new components + improve
    Added the cover element and rating system to library object array

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # library-book-app
 In this application I am using what I have learned about [[Prototype]]'s and prototypal inheritance
+
+
+Sources:
+
+Default book image: https://www.publicdomainpictures.net/en/view-image.php?image=451587&picture=vintage-book-cover-background
--- script.js
+++ script.js
@@ -1,75 +1,83 @@
-const myLibrary = [];
-const container = document.querySelector(".library-section");
+const allBooks = [];
+const bookContainer = document.querySelector(".book-shelf");
+const bookDialog = document.querySelector("#new-book-dialog");
+const addBookButton = document.querySelector(".add-book-button");
+const closeButton = document.querySelector(".close-dialog-button");
+const submitButton = document.querySelector(".submit-book-button");
+const form = document.querySelector('#new-book-form');
 
-
-function Book(title, author, pages, haveRead) {
+//Book constructor 
+function Book(title, author, pages, cover, haveRead, rating) {
     if (!new.target) {
       throw Error("You must use the 'new' operator to call the constructor");
     }
     this.title = title;
     this.author = author;
     this.pages = pages;
+    this.cover = cover;
     this.haveRead = haveRead;
+    this.rating = rating;
     this.bookId = crypto.randomUUID();
 }
 
 //Add a book to the library array 
-function addBookToLibrary(title, author, pages, haveRead) {
-  // take params, create a book then store it in the array
-  let newBook = new Book(title, author, pages, haveRead);
-  myLibrary.push(newBook);
-  //add div of latest added book
-  AddNewBookDiv(myLibrary[myLibrary.length-1]);
-}
+function addBookToLibrary(title, author, pages, cover, haveRead, rating) {
 
-//populate the library with some basic books
-function addInitialBooks(){
-  addBookToLibrary("Bone: The Great Cow Race", "Jeff Smith", 132, true);
-  addBookToLibrary("Lord of the Flies", "William Golding", 260, true);
-  addBookToLibrary("1984", "George Orwell", 300, false);
-  addBookToLibrary("Romeo and Juliet", "William Shakespeare ", 125, true);
-  addBookToLibrary("The Martian", "Andy Weir", 448, false);
-  console.table(myLibrary);
+  // take params, create a book then store it in the array
+  let newBook = new Book(title, author, pages, cover, haveRead, rating);
+  allBooks.push(newBook);
+
+  //add div of latest added book
+  AddNewBookDiv(allBooks[allBooks.length-1]);
 }
 
 
 function AddNewBookDiv(newBook){
-  // 1. Create the new div element
+
+  //Container for each book contents.
   const newBookDiv = document.createElement("div");
   newBookDiv.classList.add("book-div");
-
   //reference to book ID on div so you know which one to delete
-  newBookDiv.dataset.bookId = `${newBook.bookId}`;
+  newBookDiv.dataset.bookId = `id_${newBook.bookId}`;
+
 
   const _bookTitle = document.createElement("p");
-  _bookTitle.textContent = `${newBook.title}`;
+  _bookTitle.textContent = newBook.title;
   _bookTitle.classList.add("book-title");
   newBookDiv.appendChild(_bookTitle);
 
+  const _bookCover = document.createElement("img");
+  if (newBook.cover != '') {
+    _bookCover.setAttribute('src', newBook.cover);
+  } else {
+    _bookCover.setAttribute('src', './images/default-book-cover.jpg');
+  }
+  _bookCover.classList.add("book-cover");
+  newBookDiv.appendChild(_bookCover);
+
+
   const _bookAuthor = document.createElement("p");
-  _bookAuthor.textContent = `${newBook.author}`;
+  _bookAuthor.textContent = `by: ${newBook.author}`;
   _bookAuthor.classList.add("book-author");
   newBookDiv.appendChild(_bookAuthor);
 
   const _bookPages = document.createElement("p");
-  _bookPages.textContent = `${newBook.pages}`;
+  _bookPages.textContent = `Pages: ${newBook.pages}`;
   _bookPages.classList.add("book-pages");
   newBookDiv.appendChild(_bookPages);
 
-  const _haveRead = document.createElement("input");
-  _haveRead.type = 'checkbox';
-  _haveRead.id = `read-${newBook.title}`;
-  _haveRead.checked = newBook.haveRead === true ? true: false;
-  newBookDiv.appendChild(_haveRead);
+  const _isRead = document.createElement("input");
+  _isRead.type = 'checkbox';
+  _isRead.id = `read-${newBook.title}`;
+  _isRead.checked = newBook.haveRead === true ? true: false;
+  newBookDiv.appendChild(_isRead);
 
-  const _haveReadLabel = document.createElement("label");
-  _haveReadLabel.textContent = "Have Read";
-  _haveReadLabel.htmlFor = `read-${newBook.title}`
-  newBookDiv.appendChild(_haveReadLabel);
+  const _isReadLabel = document.createElement("label");
+  _isReadLabel.textContent = "Have Read";
+  _isReadLabel.htmlFor = `read-${newBook.title}`
+  newBookDiv.appendChild(_isReadLabel);
 
-  
   //Create Star Rating System
-  
   const starRating = document.createElement("div");
   starRating.classList.add("rating");
 
@@ -81,6 +89,7 @@ function AddNewBookDiv(newBook){
       id: `star-${i+1}-id-${newBook.bookId}`,
       value: (i+1)
     });
+    starInput.checked = (+newBook.rating === i+1)? true : false;
     starInput.classList.add("rating-radio");
     starRating.appendChild(starInput);
     
@@ -92,77 +101,60 @@ function AddNewBookDiv(newBook){
     starLabel.appendChild(starIcon);
     starRating.appendChild(starLabel);
     
-    
   }
   newBookDiv.appendChild(starRating);
   
-
-
   //Create delete button
   const deleteButton = document.createElement("button");
   deleteButton.textContent = "Delete";
-  deleteButton.addEventListener("click", (e) => {
+  deleteButton.addEventListener("click", (event) => {
     //target parent of current button being pressed to delete it. 
-    e.currentTarget.parentElement.dataset.bookId
-    if(e.currentTarget.parentElement.dataset.bookId === `${newBook.bookId}`)
+    if(event.currentTarget.parentElement.dataset.bookId === `id_${newBook.bookId}`)
     {
-      deleteBookFromLibrary(e.currentTarget.parentElement.dataset.bookId);
-      e.currentTarget.parentElement.remove();
+      deleteBookFromLibrary(event.currentTarget.parentElement.dataset.bookId);
+      event.currentTarget.parentElement.remove();
     }
   })
   newBookDiv.appendChild(deleteButton);
 
-  // 3. Append it to an existing container (like the body)
-  container.appendChild(newBookDiv);
+  //add new book to the actual book container html 
+  bookContainer.appendChild(newBookDiv);
 }
 
 //used to clean up the library when you delete a book from it
 function deleteBookFromLibrary(_bookID){
   let index = -1;
   //find index of book you are trying to delete
-  for(const _book of myLibrary){
+  for(const _book of allBooks){
     if(_book.bookId === _bookID){
-      index = myLibrary.indexOf(_book);
+      index = allBooks.indexOf(_book);
       break;
     }
   }
   if (index > -1) { 
-      myLibrary.splice(index, 1); //remove
+      allBooks.splice(index, 1); //remove
   }
-  console.table(myLibrary);
+  else{
+    console.log(`Book id: ${_bookID} could not be found`);
+  }
+  console.table(allBooks);
 }
 
-const bookDialog = document.querySelector("#book-dialog");
-const openButton = document.querySelector(".open-dialog-button");
-const closeButton = document.querySelector(".close-dialog-button");
-const submitButton = document.querySelector(".submit-book-button");
-
-openButton.addEventListener("click", () => {
-  bookDialog.showModal();
-});
-closeButton.addEventListener("click", () => {
-  bookDialog.close();
-});
-
-// JavaScript Implementation
-const form = document.querySelector('#book-form');
-
-form.addEventListener("submit", (event) => {
+//called when you submit new book form 
+function submitNewBook(event){
   // 1. Prevent the default browser page reload
   event.preventDefault(); 
   
   // 2. Instantiate FormData by passing the form element
   const formData = new FormData(event.target); 
   
-  // 4. Transform all inputs instantly into a clean JavaScript Object
-  const allData = Object.fromEntries(formData); 
-  console.log(allData); // { username: "...", email: "..." }
-
+  //Not needed
   for (let [key, value] of formData.entries()) {
     console.log(`${key}: ${value}`);
   }
 
   const bookTitle = formData.get('title-input');
+  const bookCover = formData.get('cover-input');
   const bookAuthor = formData.get('author-input');
   const bookPages = +formData.get('pages-input'); //plus constructor to convert to a number
   
@@ -172,11 +164,36 @@ form.addEventListener("submit", (event) => {
     haveRead = true;
   }
 
-  addBookToLibrary(bookTitle, bookAuthor, bookPages, haveRead);
-  console.table(myLibrary);
+  let bookRating = +formData.get('rating');
 
+  addBookToLibrary(bookTitle, bookAuthor, bookPages, bookCover, haveRead, bookRating);
+  console.table(allBooks);
+
+  bookDialog.close();
+
+  form.reset(); //clear form inputs
+}
+
+
+addBookButton.addEventListener("click", () => {
+  bookDialog.showModal();
+});
+closeButton.addEventListener("click", () => {
   bookDialog.close();
 });
 
+form.addEventListener("submit", submitNewBook);
+
+
+
+//populate the library with some basic books
+function addInitialBooks(){
+  addBookToLibrary("Bone: The Great Cow Race", "Jeff Smith", 132, "", true, 5);
+  addBookToLibrary("Lord of the Flies", "William Golding", 260, "", true, 2);
+  addBookToLibrary("1984", "George Orwell", 300, "", false, 4);
+  addBookToLibrary("Romeo and Juliet", "William Shakespeare ", 125, "", true, 3);
+  addBookToLibrary("The Martian", "Andy Weir", 448, "", false, 5);
+  console.table(allBooks);
+}
 
 addInitialBooks();
